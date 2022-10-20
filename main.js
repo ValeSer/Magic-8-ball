265336baafe428e0fcf49b8ba3a14f547551841b
Valeria Serci <balerty84@gmail.com>
2022-10-20 16:34:31 +0100

    log the question to console

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ const userName = 'Valeria';
 userName === 'Valeria'? console.log(`Hello, ${userName}!`) : console.log('Hello!')
 
 let userQuestion = 'Will I win the lottery?';
+
+console.log(`${userName} asked 'Will I win the lottery?'`)
