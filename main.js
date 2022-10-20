42d8c76a77bb8ca195d79dc3cc1b0d2d246c8bb6
Valeria Serci <balerty84@gmail.com>
2022-10-20 16:41:05 +0100

    change the way eightball get printed

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const userName = 'Valeria';
-
 userName === 'Valeria'? console.log(`Hello, ${userName}!`) : console.log('Hello!')
 
 let userQuestion = 'Will I win the lottery?';
@@ -11,28 +10,30 @@ let randomNumber = Math.floor(Math.random() * 8);
 let eightBall = ''
 
 switch (randomNumber) {
-    case 0:
-      console.log('It is certain');
-      break;
-    case 1:
-      console.log('It is decidedly so');
-      break;
-    case 2:
-      console.log('Reply hazy try again');
-      break;
-    case 3:
-      console.log('Cannot predict now');
-      break;
-    case 4:
-      console.log('Do not count on it');
-      break;
-    case 5:
-      console.log('My sources say no');
-      break;
-    case 6:
-      console.log('Outlook not so good');
-      break;
-    case 7:
-      console.log('Signs point to yes');
-      break;
-  }
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+   eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+}
+
+console.log(eightBall);
