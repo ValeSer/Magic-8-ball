90e23383726b137e4e8227ff18b396f1af6a6736
Valeria Serci <balerty84@gmail.com>
2022-10-20 16:35:32 +0100

    generate a random n between 0 and 7 and save the value

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ userName === 'Valeria'? console.log(`Hello, ${userName}!`) : console.log('Hello!
 let userQuestion = 'Will I win the lottery?';
 
 console.log(`${userName} asked 'Will I win the lottery?'`)
+
+let randomNumber = Math.floor(Math.random() * 8);
